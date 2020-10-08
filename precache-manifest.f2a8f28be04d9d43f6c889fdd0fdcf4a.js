b7a98112bce4775db9f0fe54582d1c214d59da4d
Alex Kurniawan <64827057+alexkurniawan-ng@users.noreply.github.com>
2020-10-08 21:25:08 +0700

    Updates

---
+++ precache-manifest.f2a8f28be04d9d43f6c889fdd0fdcf4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7930a58ecac941834ba2cafd02f585c",
+    "url": "/covid-react/index.html"
+  },
+  {
+    "revision": "f29963daff798a559d01",
+    "url": "/covid-react/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e55599ea792668946f89",
+    "url": "/covid-react/static/css/main.5afc7a43.chunk.css"
+  },
+  {
+    "revision": "f29963daff798a559d01",
+    "url": "/covid-react/static/js/2.c1f0e057.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-react/static/js/2.c1f0e057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55599ea792668946f89",
+    "url": "/covid-react/static/js/main.591601a2.chunk.js"
+  },
+  {
+    "revision": "50d69a2dcb8d1e543c18",
+    "url": "/covid-react/static/js/runtime-main.5134be27.js"
+  },
+  {
+    "revision": "3644ee708bf9efe747ea78b2965ca58f",
+    "url": "/covid-react/static/media/bg.3644ee70.jpg"
+  }
+]);
